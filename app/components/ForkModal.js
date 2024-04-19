f3cfb118bf69701671a1a222867cc214881e4402
m <13090422+marciob@users.noreply.github.com>
2024-04-19 12:46:58 -0300

    improving modal

--- app/components/ForkModal.js
+++ app/components/ForkModal.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { FaTimes } from "react-icons/fa";
+
 const ForkModal = ({
   isOpen,
   onClose,
@@ -7,30 +10,50 @@ const ForkModal = ({
 }) => {
   if (!isOpen) return null;
 
+  // Close modal when clicking outside
+  const handleBackdropClick = (event) => {
+    if (event.currentTarget === event.target) {
+      onClose();
+    }
+  };
+
+  // Prevent the modal from closing when clicking inside the form
+  const handleModalClick = (event) => {
+    event.stopPropagation();
+  };
+
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-800 p-5 rounded-lg">
-      <h2 className="text-lg text-white mb-2">Enter input for the new fork:</h2>
-      <form onSubmit={onForkSubmit}>
-        <input
-          type="text"
-          value={forkInput}
-          onChange={handleForkInputChange}
-          placeholder="New fork input..."
-          className="form-input mt-1 block w-full rounded-md bg-gray-700 border-gray-600"
-        />
-        <button
-          type="submit"
-          className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700"
-        >
-          Submit Fork
-        </button>
-      </form>
-      <button
-        onClick={onClose}
-        className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4"
+      onClick={handleBackdropClick}
+    >
+      <div
+        className="bg-gray-800 p-5 rounded-lg w-1/2 max-w-xl"
+        onClick={handleModalClick}
       >
-        Close
-      </button>
+        <button onClick={onClose} className="text-white float-right text-lg">
+          <FaTimes />
+        </button>
+        <h2 className="text-lg text-white mb-4">
+          Enter input for the new fork:
+        </h2>
+        <form onSubmit={onForkSubmit}>
+          <textarea
+            type="text"
+            value={forkInput}
+            onChange={handleForkInputChange}
+            placeholder="New fork input..."
+            className="form-input mt-1 block w-full h-32 rounded-md bg-gray-700 border-gray-600"
+            autoFocus
+          />
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700"
+          >
+            Submit Fork
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
